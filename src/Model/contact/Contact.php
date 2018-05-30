ecd04718c9a3546d9cf4aeb6d5c697f095b819eb
Benjamin Rose <benr242@localhost.localdomain>
2018-05-30 17:44:12 -0400

    model contact

---
+++ src/Model/contact/Contact.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benr242
+ * Date: 5/29/18
+ * Time: 8:37 PM
+ */
+
+namespace App\Model\contact;
+
+
+class Contact
+{
+    private $name;
+    private $email;
+    private $number;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param mixed $number
+     */
+    public function setNumber($number): void
+    {
+        $this->number = $number;
+    }
+
+
+}
